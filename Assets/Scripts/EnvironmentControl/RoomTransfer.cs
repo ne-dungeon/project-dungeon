b3fb3b5064ac15da12ffd9d1af3369fa70c8deb0
Anna Dunster <llama747@gmail.com>
2021-09-11 16:36:11 -0700

    Work on dialog box

--- Assets/Scripts/EnvironmentControl/RoomTransfer.cs
+++ Assets/Scripts/EnvironmentControl/RoomTransfer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomTransfer : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class RoomTransfer : MonoBehaviour
     public Vector3 playerChange;
 
     private CameraMovement cameraMovement;
+
+    public bool needPlaceCard;
+    public string placeName;
+    public GameObject textDisplay;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +25,30 @@ public class RoomTransfer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D enteree)
     {
-        if(enteree.CompareTag("Player"))
+        if (enteree.CompareTag("Player"))
         {
             cameraMovement.minPosition += cameraChangeMin;
             cameraMovement.maxPosition += cameraChangeMax;
 
             enteree.transform.position += playerChange;
+            if (needPlaceCard)
+            {
+                StartCoroutine(activatePlaceCard());
+            }
         }
+
+    }
+
+    private IEnumerator activatePlaceCard(){
+        textDisplay.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(3f);
+
+        textDisplay.SetActive(false);
     }
 }
