8fd8e6f24e10cdc5d18723298b5d1ef070a54992
Anna Dunster <llama747@gmail.com>
2021-09-15 19:46:00 -0700

    Fix room change camera weirdness

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -7,8 +7,8 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
     public float smoothing;
 
-    public Vector2 maxPosition;
     public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     // Start is called before the first frame update
     void Start()
