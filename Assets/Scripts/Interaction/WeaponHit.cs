c7f6aefed7a9b27b8d686aed8188671603f919aa
Anna Dunster <llama747@gmail.com>
2021-11-21 20:36:49 -0800

    Move weapon hit scripts to player object

--- Assets/Scripts/Interaction/WeaponHit.cs
+++ Assets/Scripts/Interaction/WeaponHit.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class WeaponHit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collisionObject)
     {
         if(collisionObject.CompareTag("Breakable")) 
